2a49e881e58ddedeb3129342073ee14ba737810b
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2025-10-12 14:30:55 +0700

    Update configuration and styles for Tailwind CSS integration and enhance index page layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://physical-computing-documentation.github.io",
-  base: "/physical-computing-documentation/",
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
